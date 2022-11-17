4453d0e7d54b5af8aa482cb3f3497c7bc8a06a91
galinavikst <galinavikst@gmail.com>
2022-11-17 22:21:34 +0100

    feat: fix forms.client.name input validation, add dateInput validation

--- order-page/order.js
+++ order-page/order.js
@@ -1,21 +1,46 @@
 const nameInput = document.forms.client.name;
 const surnameInput = document.forms.client.surname;
+const dataInput = document.forms.client.date;
+const streetInput = document.forms.address.street;
+const houseInput = document.forms.address.house;
+const flatInput = document.forms.address.flat;
 
 nameInput.onblur = function () {
-  if (nameInput.value.length <= 4) {
+  if (nameInput.value.includes(" ") || nameInput.value.length < 4)
     nameInput.className = "error";
-  }
 };
 nameInput.onfocus = () => onfocusError(nameInput);
 
-surnameInput.onblur = () => onblurError(surnameInput);
+surnameInput.onblur = function () {
+  if (surnameInput.value.includes(" ") || surnameInput.value.length < 5)
+    surnameInput.className = "error";
+};
 surnameInput.onfocus = () => onfocusError(surnameInput);
 
-function onblurError(element) {
-  if (element.value.length <= 5) {
-    element.className = "error";
-  }
-}
+streetInput.onblur = function () {
+  if (streetInput.value.length < 5) streetInput.className = "error";
+};
+streetInput.onfocus = () => onfocusError(streetInput);
+
+houseInput.onblur = function () {
+  if (isNaN(houseInput.value) || houseInput.value <= 0)
+    houseInput.className = "error";
+};
+houseInput.onfocus = () => onfocusError(houseInput);
+
+flatInput.onblur = function () {
+  if (isNaN(flatInput.value) || flatInput.value.startWith("-"))
+    flatInput.className = "error";
+};
+flatInput.onfocus = () => onfocusError(flatInput);
+
+dataInput.addEventListener("change", function () {
+  let d = new Date();
+  let inputDate = new Date(dataInput.value);
+  if (inputDate <= d) dataInput.className = "error";
+});
+dataInput.onfocus = () => onfocusError(dataInput);
+
 function onfocusError(element) {
   if (element.classList.contains("error")) {
     element.classList.remove("error");
